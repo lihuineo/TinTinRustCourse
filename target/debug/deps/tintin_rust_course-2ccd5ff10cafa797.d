c30cec620e46d09e4a036e64fe9a63b770584f0a
Li Hui <lihuineo@outlook.com>
2024-03-26 22:13:57 +0800

    add .gitignore

--- target/debug/deps/tintin_rust_course-2ccd5ff10cafa797.d
+++
@@ -1,8 +0,0 @@
-/Users/lihui40/Documents/TrainingCenter/web3/tintin_rust_course/target/debug/deps/tintin_rust_course-2ccd5ff10cafa797.rmeta: src/main.rs src/char_helper.rs src/utils/mod.rs src/utils/data_helper.rs
-
-/Users/lihui40/Documents/TrainingCenter/web3/tintin_rust_course/target/debug/deps/tintin_rust_course-2ccd5ff10cafa797.d: src/main.rs src/char_helper.rs src/utils/mod.rs src/utils/data_helper.rs
-
-src/main.rs:
-src/char_helper.rs:
-src/utils/mod.rs:
-src/utils/data_helper.rs:
